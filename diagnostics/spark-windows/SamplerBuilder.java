a5f0b8bf7c8979d4bb7c8ccb4d2b084f0da15146
frasionundead <83714125+frasionundead@users.noreply.github.com>
2026-09-14 02:54:15 +0200

    Align Windows sampler builder with spark GPL header

--- diagnostics/spark-windows/SamplerBuilder.java
+++ diagnostics/spark-windows/SamplerBuilder.java
@@ -1,8 +1,21 @@
 /*
- * This file is based on spark 1.10.53 SamplerBuilder.java.
- * Upstream: https://github.com/lucko/spark
- * Upstream commit: 8c253aec4db3450dc640c4b66f74f9c8908514fa
- * License: GNU GPL v3.0
+ * This file is part of spark.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package me.lucko.spark.common.sampler;
 
